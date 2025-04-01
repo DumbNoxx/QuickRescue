88f6b4034662bea2b7a4669a3ca53686d55d992d
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2025-04-01 01:32:43 -0400

    test

--- src/components/PrincipalPage.tsx
+++ src/components/PrincipalPage.tsx
@@ -1,19 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
 
 export const PrincipalPage = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  function alert(_arg0: string): void {
-    throw new Error("Function not implemented.");
-  }
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text style={styles.titulo}>Como estas? </Text>
       <View style={{ width: 104, margin: "auto" }}>
-        <Button title="Presioname" onPress={() => alert("Esto es un boton")} />
+        <Button
+          title="Presionamsss"
+          onPress={() => Alert.alert("Esto es un boton")}
+        />
       </View>
     </View>
   );
